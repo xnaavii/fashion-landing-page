e554caa54fc3e69d2df925e807cd84a811b43846
Ivan Petrović <petrovic.valpovo@gmail.com>
2026-01-12 19:38:59 +0000

    add slider style changes and navbar

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', function () {
+  const images = [
+    './assets/sirio-7_ZNLVlJchs-unsplash.jpg',
+    './assets/julian-myles-1RbTWZmacKA-unsplash.jpg',
+    './assets/molly-mears-4_90zmmdo_4-unsplash.jpg',
+    './assets/dom-hill-JqZlSnI2ctA-unsplash.jpg',
+    './assets/jc-gellidon-xDsq3u3ZUqc-unsplash.jpg',
+  ];
+});
